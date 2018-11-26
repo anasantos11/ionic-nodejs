86ee550e12f29b0e9f6a346aeb53dd1e5791c56f
Ana <anasantos1103@gmail.com>
2018-11-26 21:55:16 -0200

    Exibido na página inicial todos os produtos cadastrados

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,37 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ProductDTO } from '../../models/product.dto';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'page-products',
   templateUrl: 'products.html'
 })
 export class ProductsPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  products: ProductDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private productService: ProductService) {
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ProductsPage, {
-      item: item
-    });
+  ionViewWillEnter(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.findAll()
+      .subscribe(res => this.products = res);
   }
 }
